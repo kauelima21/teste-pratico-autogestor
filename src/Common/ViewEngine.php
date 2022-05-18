4171d242cf1d7951881a2a82f4127ace8a21e91e
kaue <kaueslim@outlook.com>
2022-05-17 23:22:53 -0300

    View engine created

---
+++ src/Common/ViewEngine.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kaue\BuscadorClienteAutogestor\Common;
+
+trait ViewEngine
+{
+    public function render(string $viewPath, array $data): string
+    {
+        extract($data);
+        ob_start();
+        /** Limitando para ser sempre as views da pasta 'views' e que sempre sejam arquivos php */
+        require __DIR__ . "/views/" . $viewPath . ".php";
+        return ob_get_clean();
+    }
+}
